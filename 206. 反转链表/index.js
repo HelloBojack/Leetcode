f5c5dfc2fd99454ca169df45f71f76662b0ae931
bojack <695668678@qq.com>
2021-10-09 19:41:31 +0800

    reverseList

--- 206. 反转链表/index.js
+++ 206. 反转链表/index.js
@@ -10,12 +10,32 @@
  * @return {ListNode}
  */
 var reverseList = function (head) {
-  let x = head, y = null;
-  while (x) {
-    let next = x.next
-    x.next = y;
-    y = x;
-    x = next;
-  }
-  return y
+  if (!head.next) return head
+  let next = head.next;
+  let result = reverseList(next)
+  head.next = null;
+  next.next = head;
+  return result
+  // let [pre, v] = [null, head]
+  // while (v) {
+  //   [v.next, pre, v] = [pre, v, v.next]
+  // }
+  // return pre
+  // return reverse(null, head)
 };
+var reverseList = function (head) {
+  if (!head || !head.next) return head;
+  let next = head.next; // next节点，反转后是最后一个节点
+  let reverseHead = reverseList(next);
+  head.next = null; // 裁减 head
+  next.next = head; // 尾接
+  return reverseHead;
+};
+
+// function reverse(pre, cur) {
+//   if (!cur) return pre
+//   let temp = cur.next;
+//   cur.next = pre
+
+//   return reverse(cur, temp)
+// }
