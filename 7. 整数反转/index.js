22e6e7e23c788c9aefc64905b7498f19505246b3
bojack <695668678@qq.com>
2022-01-02 22:59:27 +0800

    26. 删除有序数组中的重复项

--- 7. 整数反转/index.js
+++ 7. 整数反转/index.js
@@ -1,5 +1,17 @@
-var isPalindrome = function (x) {
-  if (x <= 0 || x % 10 == 0) return false
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+  let res = 0;
+  let sign = x < 0 ? -1 : 1;
+  x = Math.abs(x);
+  while (x > 0) {
+    res = res * 10 + x % 10;
+    x = Math.floor(x / 10);
+  }
+  if (res > Math.pow(2, 31) - 1) {
+    return 0;
+  }
+  return res * sign;
 };
-
-console.log(isPalindrome)
