8a86470a0e4a1fc82a081b5aea92e4989b3df494
bojack <695668678@qq.com>
2022-02-18 15:32:35 +0800

    141

--- 链表/141. 环形链表/index.js
+++ 链表/141. 环形链表/index.js
@@ -9,17 +9,42 @@
 /**
  * @param {ListNode} head
  * @return {boolean}
+//  */
+// var hasCycle = function (head) {
+//   var fast = head
+//   var slow = head
+//   while (fast.next) {
+//     slow = slow.next;
+//     fast = fast.next.next
+//     if (slow == fast) {
+//       return true
+//     }
+//   }
+//   return false
+// };
+// [1, 2, 3, 4]
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
  */
 var hasCycle = function (head) {
-  var fast = head
-  var slow = head
-  while (fast.next) {
-    slow = slow.next;
+  let fast = head
+  let slow = head
+  while (fast && fast.next) {
+    slow = slow.next
     fast = fast.next.next
-    if (slow == fast) {
+    if (fast == slow) {
       return true
     }
   }
   return false
 };
-[1, 2, 3, 4]
