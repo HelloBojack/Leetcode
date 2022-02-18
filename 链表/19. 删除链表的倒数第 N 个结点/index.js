5109d81cf242e428f999782fcd26d365901acb21
bojack <695668678@qq.com>
2022-02-18 14:49:30 +0800

    19

--- 链表/19. 删除链表的倒数第 N 个结点/index.js
+++ 链表/19. 删除链表的倒数第 N 个结点/index.js
@@ -10,38 +10,38 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function (head, n) {
-  var fast = head
-  var slow = head
-  var first = false
-  if (!fast.next) {
-    return null
-  }
+// var removeNthFromEnd = function (head, n) {
+//   var fast = head
+//   var slow = head
+//   var first = false
+//   if (!fast.next) {
+//     return null
+//   }
 
-  for (var i = 0; i < n; i++) {
-    if (fast.next) {
-      fast = fast.next
-    }
-    else {
-      first = true
-    }
-  }
-  if (!first) {
-    while (fast.next) {
-      fast = fast.next
-      slow = slow.next
-    }
-    if (n == 1) {
-      slow.next = null
-    } else {
-      slow.next = fast
-    }
-    return head
-  }
-  else {
-    return head.next
-  }
-};
+//   for (var i = 0; i < n; i++) {
+//     if (fast.next) {
+//       fast = fast.next
+//     }
+//     else {
+//       first = true
+//     }
+//   }
+//   if (!first) {
+//     while (fast.next) {
+//       fast = fast.next
+//       slow = slow.next
+//     }
+//     if (n == 1) {
+//       slow.next = null
+//     } else {
+//       slow.next = fast
+//     }
+//     return head
+//   }
+//   else {
+//     return head.next
+//   }
+// };
 
 // [1,2],2 -> [2]
 // fast=1,slow=1
@@ -69,3 +69,34 @@ var removeNthFromEnd = function (head, n) {
 // fast=3
 // fast=4,slow=2
 // fast=5,slow=3
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  let temp = new ListNode()
+  temp.next = head
+  let l1 = temp
+  let l2 = temp
+
+
+  for (let i = 0; i < n + 1; i++) {
+    l1 = l1.next;
+  }
+  while (l1) {
+    l1 = l1.next;
+    l2 = l2.next;
+  }
+  l2.next = l2.next.next;
+  return temp.next
+};
