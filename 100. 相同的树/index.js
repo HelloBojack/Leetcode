3bb0736da76c7418eef187a6cd897e0db2ccaeb7
bojack <xlk970115@gmail.com>
2022-10-14 00:14:02 +0800

    100. 相同的树

---
+++ 100. 相同的树/index.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var traversal = function (p, q) {};
+
+var isSameTree = function (p, q) {
+  if (!p && !q) {
+    return true;
+  } else if (p && !q) {
+    return false;
+  } else if (!p && q) {
+    return false;
+  } else {
+    return (
+      p.val == q.val &&
+      isSameTree(p.left, q.left) &&
+      isSameTree(p.right, q.right)
+    );
+  }
+};
